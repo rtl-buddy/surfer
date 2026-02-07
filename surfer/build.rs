90b2336c23fd153b6d585b3ed57e7eb8b96b7245
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2026-02-07 21:25:00 +0100

    Move icon creation to build

--- surfer/build.rs
+++ surfer/build.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::path::Path;
 use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -12,5 +13,39 @@ fn main() -> Result<(), Box<dyn Error>> {
         .add_instructions(&build)?
         .add_instructions(&git)?
         .emit()?;
+
+    println!("cargo:rerun-if-changed=assets/com.gitlab.surferproject.surfer.png");
+
+    // Load and process the icon at build time
+    let icon_bytes = include_bytes!("assets/com.gitlab.surferproject.surfer.png");
+    let decoder = png::Decoder::new(std::io::Cursor::new(&icon_bytes[..]));
+    let mut reader = decoder.read_info().expect("Failed to read PNG info");
+    let mut rgba_data = vec![
+        0;
+        reader
+            .output_buffer_size()
+            .expect("Failed to calculate buffer size")
+    ];
+    let info = reader
+        .next_frame(&mut rgba_data)
+        .expect("Failed to decode PNG");
+    let (width, height) = (info.width, info.height);
+
+    // Ensure we have RGBA8 format
+    rgba_data.truncate(info.buffer_size());
+
+    let icon_data = egui::viewport::IconData {
+        rgba: rgba_data,
+        width,
+        height,
+    };
+
+    let serialized = bincode::serialize(&icon_data).expect("Failed to serialize icon data");
+
+    // Write binary icon data for compile-time inclusion
+    let out_dir = std::env::var("OUT_DIR")?;
+    let dest_path = Path::new(&out_dir).join("icon_data.bin");
+    std::fs::write(&dest_path, serialized)?;
+
     Ok(())
 }
--- surfer/src/main.rs
+++ surfer/src/main.rs
@@ -19,6 +19,8 @@ mod main_impl {
     };
     use tracing::error;
 
+    const ICON_DATA_BYTES: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/icon_data.bin"));
+
     #[derive(clap::Subcommand)]
     enum Commands {
         #[cfg(not(target_arch = "wasm32"))]
@@ -201,22 +203,15 @@ mod main_impl {
             }
             _ => None,
         };
-        let icon = image::load_from_memory_with_format(
-            include_bytes!("../assets/com.gitlab.surferproject.surfer.png"),
-            image::ImageFormat::Png,
-        )
-        .expect("Failed to open icon path")
-        .to_rgba8();
-        let (icon_width, icon_height) = icon.dimensions();
+
         let options = eframe::NativeOptions {
             viewport: egui::ViewportBuilder::default()
                 .with_app_id("org.surfer-project.surfer")
                 .with_title("Surfer")
-                .with_icon(egui::viewport::IconData {
-                    rgba: icon.into_raw(),
-                    width: icon_width,
-                    height: icon_height,
-                })
+                .with_icon(
+                    bincode::deserialize::<egui::viewport::IconData>(ICON_DATA_BYTES)
+                        .expect("Failed to deserialize icon data"),
+                )
                 .with_inner_size(Vec2::new(
                     state.user.config.layout.window_width as f32,
                     state.user.config.layout.window_height as f32,

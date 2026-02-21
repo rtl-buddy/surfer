1cc57530ad9015b58790238dd2c0db5601850fa0
TheZoq2 <mail@fransskarman.com>
2026-02-21 10:17:27 +0100

    Add commands for analog settings

--- libsurfer/src/command_parser.rs
+++ libsurfer/src/command_parser.rs
@@ -4,6 +4,7 @@ use std::sync::LazyLock;
 use std::{fs, str::FromStr};
 
 use crate::config::ArrowKeyBindings;
+use crate::displayed_item::{AnalogRenderStyle, AnalogSettings, AnalogYAxisScale};
 use crate::displayed_item_tree::{Node, VisibleItemIndex};
 use crate::fzcmd::{Command, ParamGreed};
 use crate::hierarchy::HierarchyStyle;
@@ -171,6 +172,15 @@ pub fn get_parser(state: &SystemState) -> Command<Message> {
         .as_ref()
         .is_some_and(|w| w.inner.is_transactions());
 
+    let item_heights = state
+        .user
+        .config
+        .layout
+        .waveforms_line_height_multiples
+        .iter()
+        .map(|height| format!("{height}"))
+        .collect::<Vec<_>>();
+
     fn files_with_ext(matches: fn(&str) -> bool) -> Vec<String> {
         if let Ok(res) = fs::read_dir(".") {
             res.map(|res| res.map(|e| e.path()).unwrap_or_default())
@@ -256,6 +266,8 @@ pub fn get_parser(state: &SystemState) -> Command<Message> {
             "item_set_color",
             "item_set_background_color",
             "item_set_format",
+            "item_set_analog",
+            "item_set_height",
             "item_unset_color",
             "item_unset_background_color",
             "item_unfocus",
@@ -619,6 +631,49 @@ pub fn get_parser(state: &SystemState) -> Command<Message> {
                         Some(Command::Terminal(Message::VariableFormatChange(
                             MessageTarget::CurrentSelection,
                             word.to_string(),
+                        )))
+                    }),
+                ),
+                "item_set_analog" => single_word(
+                    vec![
+                        "off".to_string(),
+                        "step_viewport".to_string(),
+                        "step_global".to_string(),
+                        "interpolated_viewport".to_string(),
+                        "interpolated_global".to_string(),
+                    ],
+                    Box::new(|word| {
+                        Some(Command::Terminal(Message::SetAnalogSettings(
+                            MessageTarget::CurrentSelection,
+                            match word {
+                                "off" => None,
+                                "step_viewport" => Some(AnalogSettings {
+                                    render_style: AnalogRenderStyle::Step,
+                                    y_axis_scale: AnalogYAxisScale::Viewport,
+                                }),
+                                "step_global" => Some(AnalogSettings {
+                                    render_style: AnalogRenderStyle::Step,
+                                    y_axis_scale: AnalogYAxisScale::Global,
+                                }),
+                                "interpolated_viewport" => Some(AnalogSettings {
+                                    render_style: AnalogRenderStyle::Interpolated,
+                                    y_axis_scale: AnalogYAxisScale::Viewport,
+                                }),
+                                "interpolated_global" => Some(AnalogSettings {
+                                    render_style: AnalogRenderStyle::Interpolated,
+                                    y_axis_scale: AnalogYAxisScale::Global,
+                                }),
+                                _ => None,
+                            },
+                        )))
+                    }),
+                ),
+                "item_set_height" => single_word(
+                    item_heights.clone(),
+                    Box::new(|word| {
+                        Some(Command::Terminal(Message::ItemHeightScalingFactorChange(
+                            MessageTarget::CurrentSelection,
+                            word.parse::<f32>().unwrap_or_default(),
                         )))
                     }),
                 ),

4a9ed9bed5ced9bd4639ad9149de0e7884b53f7b
Deekshita Kutikuppala <deekshita.kutikuppala5@gmail.com>
2026-04-08 01:10:48 +0530

    Set save state dialog directory from waveform path

--- libsurfer/src/file_dialog.rs
+++ libsurfer/src/file_dialog.rs
@@ -61,6 +61,7 @@ impl SystemState {
         &mut self,
         title: &'static str,
         filter: (String, Vec<String>),
+        initial_directory: Option<PathBuf>,
         messages: F,
     ) where
         F: FnOnce(FileHandle) -> Fut + Send + 'static,
@@ -69,7 +70,13 @@ impl SystemState {
         let sender = self.channels.msg_sender.clone();
 
         perform_async_work(async move {
-            if let Some(file) = create_file_dialog(filter, title).save_file().await {
+            let mut dialog = create_file_dialog(filter, title);
+
+            if let Some(directory) = initial_directory {
+                dialog = dialog.set_directory(directory);
+            }
+
+            if let Some(file) = dialog.save_file().await {
                 checked_send_many(&sender, messages(file).await);
             }
         });
@@ -80,6 +87,7 @@ impl SystemState {
         &mut self,
         title: &'static str,
         filter: (String, Vec<String>),
+        _initial_directory: Option<std::path::PathBuf>,
         messages: F,
     ) where
         F: FnOnce(FileHandle) -> Fut + 'static,
